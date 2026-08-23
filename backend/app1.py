cc2c35466ef77fe0d207b2f56409cc3051677f3e
prachimagadum06 <prachimagadum06@gmail.com>
2026-08-23 10:06:18 +0530

    update

--- backend/app1.py
+++ backend/app1.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
-client = MongoClient("YOUR_MONGODB_CONNECTION_STRING")
+client = MongoClient("mongodb+srv://mtanvi528_db_user:mtanvi528_db_user@cluster0.ye0o5h5.mongodb.net/?appName=Cluster0")
 db = client["todo_database"]
 collection = db["todo_items"]
 
@@ -22,3 +23,6 @@ def submit_todo_item():
     return jsonify({
         "message": "To-Do item submitted successfully"
     }), 201
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
